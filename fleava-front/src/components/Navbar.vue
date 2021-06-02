81c6987caec291d95ea61545090db11db0c6adcf
unknown <Pabeloszh@gmail.com>
2021-06-02 08:42:21 +0200

    first commit

---
+++ fleava-front/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="navbar">
+        <a href="">
+            <img src="https://fleava.com/assets/img/general/fleava-logo.svg" alt="logo">
+        </a>
+        <div>
+            <a href="">ABOUT</a>
+            <a href="">WORK</a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+    .navbar{
+        position:fixed;
+        top:160px;
+    }
+</style>
---
+++ fleave-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ecfd0ebe-be75-4d0b-9c44-c3d4907f83c4'
+};
