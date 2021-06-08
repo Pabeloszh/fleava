ac81e780fecacdc3dd1d99a2a6e879d956d34fdf
unknown <Pabeloszh@gmail.com>
2021-06-08 13:12:40 +0200

    mobile touch events added

--- fleava-front/src/pages/Home/Expertise/Expertise.vue
+++ fleava-front/src/pages/Home/Expertise/Expertise.vue
@@ -7,15 +7,17 @@
                 <p>What we do -></p>
             </div>
         </div>
-        <div class="carousel">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+        <div class="carousel-container" @touchstart="touchStart" @touchmove="touchMove" @touchend="()=>touched=false" @mousedown="carouselOnDragg" @mouseup="()=>pressed = false" @mousemove="carouselOnSlide">
+            <div class="carousel">
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
         </div>
     </div>
 </template>
@@ -24,6 +26,59 @@ import Card from "./Card/Card"
 export default {
     components:{
         Card
+    },
+    data(){
+        return{
+            pressed: false,
+            touched: false,
+            touch: 0,
+            prevTouchX: 0,
+            prevMoveX: 0,
+            prevxUp: 0,
+            x: 0,
+            mobileX:0,
+        }
+    },
+    methods:{
+        carouselOnDragg(e) {
+            this.pressed = true;
+        },
+        carouselOnSlide(e){
+            if(!this.pressed) return
+            if(this.x > 0){
+                this.x = 0;
+                document.querySelector('.carousel').style.transform = `translateX(0px)`;
+                return
+
+            }
+            e.preventDefault();
+            this.x += e.movementX;
+            document.querySelector('.carousel').style.transform = `translateX(${this.x}px)`;
+        },
+        touchStart(e){
+            this.touched = true;
+            this.prevTouchX = e.targetTouches[0].clientX;
+        },
+        touchMove(e){
+            if(!this.touched){
+                return
+            }
+
+            let xUp = e.touches[0].clientX;
+            this.prevxUp = e.touches[0].pageX;
+
+            if(xUp >= this.prevMoveX) {
+                console.log(Math.abs(this.prevTouchX - this.prevMoveX))
+                this.prevMoveX !== 0 && (this.mobileX += Math.abs(this.prevTouchX - this.prevMoveX))
+            }
+            if(xUp <= this.prevMoveX) {
+                console.log(Math.abs(this.prevTouchX - this.prevMoveX))
+                this.prevMoveX !== 0 && (this.mobileX -= Math.abs(this.prevTouchX - this.prevMoveX))
+            }
+            document.querySelector('.carousel').style.transform = `translateX(${this.mobileX / 10}px)`;
+            
+            this.prevMoveX = xUp
+        }
     }
 }
 </script>
