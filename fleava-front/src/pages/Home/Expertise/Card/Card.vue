240439b00a7900832c178b1919088806d4423915
unknown <Pabeloszh@gmail.com>
2021-06-08 08:01:08 +0200

    expertise container with cards

---
+++ fleava-front/src/pages/Home/Expertise/Card/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card-container">
+        <img src="https://fleava.com/assets/img/expertise/thumbs/strategy.webp" alt="">
+        <div class="desc">
+            <h3>/ 01</h3>
+            <div>
+                <h2>Digital stategy</h2>
+                <p>Consumer Insights & Trends, Go-To-Market Planning, Data Science & Analytics, Product Strategy & Roadmapping.</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped src="./Card.style.scss">
+</style>
---
+++ fleava-front/src/pages/Home/Expertise/Expertise.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="expertise-container">
+        <div class="desc">
+            <h3>/ Expertise</h3>
+            <div>
+                <h2>Elevating <span>Brands</span> through<br/>innovation in Digital<br/>Transformation.</h2>
+                <p>What we do -></p>
+            </div>
+        </div>
+        <div class="carousel">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+    </div>
+</template>
+<script>
+import Card from "./Card/Card"
+export default {
+    components:{
+        Card
+    }
+}
+</script>
+<style lang="scss" scoped src="./Expertise.style.scss">
+</style>
