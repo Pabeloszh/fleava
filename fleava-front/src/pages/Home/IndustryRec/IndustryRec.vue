b6eb7b91e8d8bad4cf907bf9c51b5d343bcc59d6
unknown <Pabeloszh@gmail.com>
2021-06-09 15:27:41 +0200

    industry recognition component added

---
+++ fleava-front/src/pages/Home/IndustryRec/IndustryRec.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="industry-container">
+        <div class="desc">
+            <div>
+                <h3>/ industry recognition</h3>
+                <h2>Nominated as Agency of the Year amongst best Digital Agencies Worldwide.</h2>
+            </div>
+            <img src="https://fleava.com/assets/img/awards/badge.webp" alt="">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped src="./IndustryRec.style.scss">
+</style>
