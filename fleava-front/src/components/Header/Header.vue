2e8afb8af04f14368511001d0807b21b4836b5a0
unknown <Pabeloszh@gmail.com>
2021-06-11 07:55:29 +0200

    components directory in front changed

---
+++ fleava-front/src/components/Header/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="header-container">
+        <span class="reveal-item">/ Expertise</span>
+        <div class="title">
+            <span class="reveal-item"></span>
+            <span class="reveal-item"></span>
+            <span class="reveal-item"></span>
+        </div>
+        <span class="reveal-item"><p>Scroll down</p></span>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped src="./Header.style.scss">
+</style>
