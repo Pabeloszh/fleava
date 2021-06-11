89394ee0be44a582080982124271d6df0ed9395b
unknown <Pabeloszh@gmail.com>
2021-06-11 13:21:31 +0200

    works, work card and work header created

---
+++ fleava-front/src/pages/works/[name].vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+      <WorkHeader :wHeaderData="wHeaderData"/>
+        <div class="work-container">
+            
+        </div>
+  </Layout>
+</template>
+
+<script>
+import WorkHeader from "@/routes/Works[name]/WorkHeader/WorkHeader"
+export default {
+    components:{
+        WorkHeader
+    },
+    data(){
+        return{
+            wHeaderData: {
+                mainImg: 'https://fleava.com/assets/img/works/raia/banner.webp',
+                dir: 'work',
+                name: "club raia",
+                title1: "Revolutionizing the",
+                title2: "Nightlife Experience.",
+            }
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ fleava-front/src/routes/Works/WCardsCont/WCardsCont.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="wcards-container">
+        <div>
+            <WorkCard v-for="card in cards" :key="card" :num="card"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import WorkCard from "@/routes/Works/WorkCard/WorkCard"
+export default {
+    components:{
+        WorkCard
+    },
+    data(){
+        return{
+            cards: [1,2,3,4,5,6]
+
+        }
+    }
+}
+</script>
+<style lang="scss" scoped src="./WCardCont.style.scss">
+</style>
