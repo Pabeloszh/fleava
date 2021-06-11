d96575e9b5fcafe009f68e9fa5e26bf9aa4f2ca8
unknown <Pabeloszh@gmail.com>
2021-06-11 13:20:17 +0200

    header created

--- fleava-front/src/components/Header/Header.vue
+++ fleava-front/src/components/Header/Header.vue
@@ -1,18 +1,34 @@
 <template>
-    <div class="header-container">
-        <span class="reveal-item">/ Expertise</span>
-        <div class="title">
-            <span class="reveal-item"></span>
-            <span class="reveal-item"></span>
-            <span class="reveal-item"></span>
+    <div class="header-container" >
+        <div class="header" ref="header">
+            <div class="reveal-item" v-if="headerData.name"><h3>/ {{headerData.name}}</h3></div>
+            <div class="title">
+                <div class="reveal-item"><h1>{{headerData.title1}}</h1></div>
+                <div class="reveal-item"><h1>{{headerData.title2}}</h1></div>
+                <div class="reveal-item"><h1>{{headerData.title3}}</h1></div>
+            </div>
+            <div class="reveal-item"><p>Scroll down</p></div>
         </div>
-        <span class="reveal-item"><p>Scroll down</p></span>
     </div>
 </template>
-
 <script>
 export default {
-
+    data: () => ({
+        observer: null,
+    }),
+    props:{
+        headerData: Object,
+    },
+    mounted() {
+        this.observer = new IntersectionObserver((entries)=> {
+            entries.forEach(entry => {
+                if(!entry.isIntersecting) return;
+               this.$refs.header.classList.add('header-active');
+               this.observer.unobserve(entry.target);
+            })
+        });
+        this.observer.observe(this.$refs.header);
+    },
 }
 </script>
 
