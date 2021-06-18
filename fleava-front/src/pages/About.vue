f60934b1b4e1ac3daf5c3965d10ad5f7bdbd81b8
unknown <Pabeloszh@gmail.com>
2021-06-18 13:54:05 +0200

    about desc component added

--- fleava-front/src/pages/About.vue
+++ fleava-front/src/pages/About.vue
@@ -1,18 +1,18 @@
 <template>
     <Layout>
         <Header/>
+        <AboutDesc/>
     </Layout>
 </template>
-
 <script>
-import Header from "@/routes/About/Header/Header"
+import Header from "@/routes/About/Header/Header";
+import AboutDesc from "../routes/About/AboutDesc/AboutDesc.vue";
 export default {
     components:{
-        Header
+        Header,
+        AboutDesc
     }
 }
 </script>
-
 <style>
-
 </style>
