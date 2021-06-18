2248f7d543310fcec648439fe841b681e8d9a73e
unknown <Pabeloszh@gmail.com>
2021-06-18 13:28:33 +0200

    about header created

---
+++ fleava-front/src/pages/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <Layout>
+        <Header/>
+    </Layout>
+</template>
+
+<script>
+import Header from "@/routes/About/Header/Header"
+export default {
+    components:{
+        Header
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ fleava-front/src/routes/About/Header/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header-container">
+      <div class="hello" ref="hello">
+        <span>Hello.</span>
+      </div>
+      <div class="parallax" ref="prlx">
+          <g-image src="~/images/banner.webp" alt="banner_image"/>
+      </div>
+      <div class="header" ref="header">
+          <div class="reveal-item"><h3>/ about</h3></div>
+          <div class="title">
+            <div class="reveal-item">
+                <h1>We are Fleava. We create </h1>
+            </div>
+            <div class="reveal-item">
+                <h1>award-winning<br/>Products,</h1>
+            </div>
+            <div class="reveal-item">
+                <h1>memorable Campaigns,</h1>
+            </div>
+            <div class="reveal-item">
+                <h1>& remarkable Brands</h1>
+            </div>
+          </div>
+          <div class="reveal-item"><p>Scroll down</p></div>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+    data: () => ({
+        observer: null,
+    }),
+    props:{
+        headerData: Object,
+    },
+    mounted() {
+        this.observer = new IntersectionObserver((entries)=> {
+            entries.forEach(entry => {
+                if(!entry.isIntersecting) return;
+               this.$refs.hello.classList.add('hello-active');
+               setTimeout(()=> this.$refs.header.classList.add('header-active'), 2000);
+               setTimeout(()=> this.$refs.prlx.classList.add('parallax-active'), 2000);
+               this.observer.unobserve(entry.target);
+            })
+        });
+        this.observer.observe(this.$refs.header);
+    },
+}
+</script>
+<style lang="scss" scoped src="./Header.style.scss">
+</style>
