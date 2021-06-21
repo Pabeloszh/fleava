d62cc44e270194484f857538f8b2b86d02288bb0
unknown <Pabeloszh@gmail.com>
2021-06-21 12:56:59 +0200

    creating reusable component instead of using wcardcontainer and wcard

---
+++ fleava-front/src/components/CardsContainer/CardsContainer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="cards-container">
+        <div>
+            <Card v-for="edge in data" :key="edge.node.id" :link="edge.node.name" :data="edge.node" :route="route"/>
+        </div>
+    </div>
+</template>
+<script>
+import Card from "@/components/Card/Card"
+export default {
+    components:{
+        Card
+    },
+    props:{
+        data: Array,
+        route: String,
+    },
+    mounted(){
+        console.log(this.data)
+    }
+}
+</script>
+<style lang="scss" scoped src="./CardsContainer.style.scss">
+</style>
---
+++ fleava-front/src/pages/Expertise.vue
@@ -0,0 +1,42 @@
+<template>
+  <Layout>
+    <Header :headerData="headerData"/>
+    <CardsContainer :data="$page.exp.edges" :route="`expertise`"/>
+  </Layout>
+</template>
+<page-query>
+    query{
+        exp: allPostedExpertise{
+            edges{
+                node{
+                    id,
+                    name,
+                    title,
+                    cardImg
+                }
+            }
+        }
+    }
+</page-query>
+<script>
+import Header from "@/components/Header/Header"
+import CardsContainer from "@/components/CardsContainer/CardsContainer"
+export default {
+    components:{
+        Header,
+        CardsContainer
+    },
+    data(){
+        return{
+            headerData: {
+                name: "expertise",
+                title1: "Elevating Brands through",
+                title2: "innovation in Digital",
+                title3: "Transformation."
+            }
+        }
+    },
+}
+</script>
+<style>
+</style>
