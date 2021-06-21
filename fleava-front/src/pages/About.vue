bab3d83ab1780b651c62474a226f55d19d45f898
unknown <Pabeloszh@gmail.com>
2021-06-21 08:36:32 +0200

    about banner added

--- fleava-front/src/pages/About.vue
+++ fleava-front/src/pages/About.vue
@@ -2,15 +2,18 @@
     <Layout>
         <Header/>
         <AboutDesc/>
+        <AboutBanner/>
     </Layout>
 </template>
 <script>
 import Header from "@/routes/About/Header/Header";
-import AboutDesc from "../routes/About/AboutDesc/AboutDesc.vue";
+import AboutDesc from "@/routes/About/AboutDesc/AboutDesc.vue";
+import AboutBanner from "@/routes/About/AboutBanner/AboutBanner";
 export default {
     components:{
         Header,
-        AboutDesc
+        AboutDesc,
+        AboutBanner
     }
 }
 </script>
