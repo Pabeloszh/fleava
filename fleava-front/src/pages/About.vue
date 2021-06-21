4a8b29640d5e650d448e37e42d8c0f16e8fdc749
unknown <Pabeloszh@gmail.com>
2021-06-21 11:14:03 +0200

    brands component added to about route

--- fleava-front/src/pages/About.vue
+++ fleava-front/src/pages/About.vue
@@ -3,17 +3,20 @@
         <Header/>
         <AboutDesc/>
         <AboutBanner/>
+        <AboutBrands/>
     </Layout>
 </template>
 <script>
 import Header from "@/routes/About/Header/Header";
 import AboutDesc from "@/routes/About/AboutDesc/AboutDesc.vue";
 import AboutBanner from "@/routes/About/AboutBanner/AboutBanner";
+import AboutBrands from "@/routes/About/AboutBrands/AboutBrands"
 export default {
     components:{
         Header,
         AboutDesc,
-        AboutBanner
+        AboutBanner,
+        AboutBrands
     }
 }
 </script>
