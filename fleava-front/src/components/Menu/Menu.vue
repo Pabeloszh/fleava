33bde2911ed8fcf8d8f238028742ca335e094998
unknown <Pabeloszh@gmail.com>
2021-06-30 13:07:28 +0200

    nav links and menu sup numbers fixed

--- fleava-front/src/components/Menu/Menu.vue
+++ fleava-front/src/components/Menu/Menu.vue
@@ -3,31 +3,31 @@
     <div class="grayScale"></div>
     <div class="overlay">
         <div class="main">
-            <h1>Home</h1>
-            <h1>Works <sup>10</sup></h1>
-            <h1>Expertise <sup>06</sup></h1>
-            <h1>About</h1>
-            <h1>Journal <sup>19</sup></h1>
+            <h1 @click="$router.push('/')">Home</h1>
+            <h1 @click="$router.push('/works')">Works <sup v-if="$static.works.totalCount">{{$static.works.totalCount > 10 ? $static.works.totalCount : "0" + $static.works.totalCount}}</sup></h1>
+            <h1 @click="$router.push('/expertise')">Expertise <sup v-if="$static.works.totalCount">{{$static.expertise.totalCount > 10 ? $static.expertise.totalCount : "0" + $static.expertise.totalCount}}</sup></h1>
+            <h1 @click="$router.push('/about')">About</h1>
+            <h1 @click="$router.push('/journal')">Journal <sup v-if="$static.works.totalCount">{{$static.journal.totalCount > 10 ? $static.journal.totalCount : "0" + $static.journal.totalCount}}</sup></h1>
         </div>
         <div>
             <div class="location">
-                <div>
+                <div @click="$router.push('/singapore')">
                     <h3>Singapore</h3>
                     <p>160 Robinson Road, #14-04 Singapore Business Federation Centre - 068914, Singapore</p>
                 </div>
-                <div>
+                <div @click="$router.push('/bali')">
                     <h3>Bali</h3>
                     <p>Jalan Merta Agung, No.25, Kerobokan Kelod, Badung, Bali - 80361, Indonesia</p>
                 </div>
             </div>
             <div class="sec">
                 <div>
-                    <h3>Awards <sup>43</sup></h3>
-                    <h3>Brands <sup>103</sup></h3>
-                    <h3>Careers <sup>04</sup></h3>
+                    <h3 @click="$router.push('/awards')">Awards <sup>43</sup></h3>
+                    <h3 @click="$router.push('/brands')">Brands <sup>103</sup></h3>
+                    <h3 @click="$router.push('/careers')">Careers <sup v-if="$static.careers.totalCount">{{$static.careers.totalCount > 10 ? $static.careers.totalCount : "0" + $static.careers.totalCount}}</sup></h3>
                 </div>
                 <div>
-                    <h3>Inquiries</h3>
+                    <h3 @click="$router.push('/')">Inquiries</h3>
                     <h3>Contact Us</h3>
                     <h3>Transform</h3>
                 </div>
@@ -59,6 +59,22 @@
     </div>
 </div>
 </template>
+<static-query>
+    query{
+        works: allPostedWorks{
+            totalCount
+        }
+        expertise: allPostedExpertise{
+            totalCount
+        }
+        journal: allPostedJournal{
+            totalCount
+        }
+        careers: allPostedCareers{
+            totalCount
+        }
+    }
+</static-query>
 <script>
 export default {
     methods: {
@@ -67,6 +83,9 @@ export default {
             document.querySelector('.grayScale').classList.toggle('grayScale-active');
             document.querySelector('.overlay').classList.toggle('overlay-active');
         }
+    },
+    mounted(){
+        console.log(this.$static.works.totalCount)
     }
 }
 </script>
